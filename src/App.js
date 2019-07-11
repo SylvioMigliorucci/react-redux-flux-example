4788dd8cad0dab7ef8ca52a421f08dabf71291a1
Sylvio <sylvio.migliorucci@gmail.com>
2019-07-10 23:20:23 -0300

    config flux fail

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import {BrowserRouter} from 'react-router-dom'
-import Routes from './routes'
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './routes';
+import Header from './components/Header/index';
 
 function App() {
-  return (
-    <BrowserRouter>
-        <Header/>
-        <Routes />
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes />
+        </BrowserRouter>
+    );
 }
 
 export default App;
